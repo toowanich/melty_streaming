e0f8e455ebe8872d33dfa43cfdb3615290a5d0d8
Wanich Keatkajonjumroen <toowanich@gmail.com>
2021-08-30 18:18:17 +0700

    modified to be character sidebars

--- melty_streaming/static/js/index.js
+++ melty_streaming/static/js/index.js
@@ -3,82 +3,18 @@ function handlePlayer(data, plr)
 {
     if (!data.waiting) {
         charId = data[`p${plr}char`].toString().padStart(2, "0");
-
-        // Set the name of this character
-        $(`#p${plr}name`).css("background-image",
-            `url("assets/name/vs_name00_${charId}.png")`);
-
-        // Character Selected
-        if(data[`p${plr}selmode`] > 0) {
-            // Set the mugshot of this character
-            $(`#p${plr}mug`).css("background-image",
-                `url("assets/mug/cut_${charId}00.png")`);
-            $(`#p${plr}mugheat`).css("background-image",
-                `url("assets/mug/cut_${charId}00.png")`);
-
-            // Set if this mugshot is selected
-            $(`#p${plr}mug`).toggleClass("selected", true);
-            $(`#p${plr}bnr`).toggleClass("selected", true);
-
-            // If the character is a duo character, set their partner
-            if([0x04, 0x22, 0x23].includes(data[`p${plr}char`])) {
-                $(`#p${plr}mugptnr`).css("background-image",
-                    `url("assets/mug/cut_${charId}01.png")`);
-                $(`#p${plr}mugheat`).css("background-image",
-                    `url("assets/mug/cut_${charId}00.png"), url("assets/mug/cut_${charId}01.png")`);
-                $(`#p${plr}mugptnr`).toggleClass("selected", true);
-            }
-        } else {
-            // Toggle the selected class off
-            $(`#p${plr}mug`).toggleClass("selected", false);
-            $(`#p${plr}bnr`).toggleClass("selected", false);
-            $(`#p${plr}mugptnr`).toggleClass("selected", false);
-        }
-
-        // Moon Selected
-        if(data[`p${plr}selmode`] > 1) {
-            // Set the moon image
-            $(`#p${plr}moon`).css("background-image",
-                `url("assets/moon/moon_${data[`p${plr}moon`]}.png")`);
-
-            // Set if the moon is selected
-            $(`#p${plr}moon`).toggleClass("selected", true);
-        } else {
-            $(`#p${plr}moon`).toggleClass("selected", false);
-            $(`#p${plr}moon`).css("background-image",
-                `none`);
-        }
-
-        // Check for heat type
-        if (data[`p${plr}heat`] !== 0 && data[`gamemode`] === 1) {
-            // Check if the character is in Blood Heat
-            if (data[`p${plr}heat`] === 3) {
-                $(`#p${plr}mugheat`).toggleClass("bloodheatmode", true);
-            // Heat and Max mode use the same colors
-            } else {
-                $(`#p${plr}mugheat`).toggleClass("heatmode", true);
-            }
-        } else {
-            $(`#p${plr}mugheat`).toggleClass("heatmode", false);
-            $(`#p${plr}mugheat`).toggleClass("bloodheatmode", false);
+        //Sets character sidebars when game goes to chara intros
+        if(data["gamemode"] == 1){
+            $(`#p${plr}side`).css("background-image",
+                `url("assets/sidebar/apm_com_${charId}_${plr}p_${data[`p${plr}moon`]}.bmp")`);
+        }else{
+            $(`#p${plr}side`).css("background-image",
+                `url("assets/sidebar/apm_com_default_${plr}p.bmp")`);
         }
     } else {
-        // Toggle the selected class off
-        $(`#p${plr}mug`).toggleClass("selected", false);
-        $(`#p${plr}bnr`).toggleClass("selected", false);
-        $(`#p${plr}mugptnr`).toggleClass("selected", false);
 
-        $(`#p${plr}moon`).toggleClass("selected", false);
-        $(`#p${plr}moon`).css("background-image",
-            `none`);
-
-        // Set the name to none
-        $(`#p${plr}name`).css("background-image",
-            `none`);
-
-        // Turn off heat modes
-        $(`#p${plr}mugheat`).toggleClass("heatmode", false);
-        $(`#p${plr}mugheat`).toggleClass("bloodheatmode", false);
+        $(`#p${plr}side`).css("background-image",
+                `url("assets/sidebar/apm_com_default_${plr}p.bmp")`);
     }
 }
 
